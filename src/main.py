89d4ebcb8b806e7671c424c610e16429071579fe
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-29 16:05:06 -0500

    Create grid

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 import pyray
 
-pyray.init_window(800, 450, "")
+BLANK, UP, RIGHT, DOWN, LEFT = range(5)
 
+WIDTH = 800
+HEIGHT = 450
+
+pyray.init_window(WIDTH, HEIGHT, "")
 
 blank = pyray.load_texture_from_image(pyray.load_image("./tiles/blank.png"))
 up = pyray.load_texture_from_image(pyray.load_image("./tiles/up.png"))
@@ -9,6 +13,25 @@ right = pyray.load_texture_from_image(pyray.load_image("./tiles/right.png"))
 down = pyray.load_texture_from_image(pyray.load_image("./tiles/down.png"))
 left = pyray.load_texture_from_image(pyray.load_image("./tiles/left.png"))
 
+N = 4
+
+grid = [
+    [
+        {
+            "collapsed": False,
+            "options": [
+                BLANK,
+                UP,
+                RIGHT,
+                DOWN,
+                LEFT,
+            ],
+        }
+        for _ in range(N)
+    ]
+    for _ in range(N)
+]
+print(grid)
 
 while not pyray.window_should_close():
     pyray.begin_drawing()
