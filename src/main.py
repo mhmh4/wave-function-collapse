d17d8f33ebd400061a7c7091a6bcf65c700ee3f7
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-29 14:53:43 -0500

    Load tiles

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 import pyray
 
-pyray.init_window(800, 450, "Hello")
+pyray.init_window(800, 450, "")
+
+
+blank = pyray.load_texture_from_image(pyray.load_image("./tiles/blank.png"))
+up = pyray.load_texture_from_image(pyray.load_image("./tiles/up.png"))
+right = pyray.load_texture_from_image(pyray.load_image("./tiles/right.png"))
+down = pyray.load_texture_from_image(pyray.load_image("./tiles/down.png"))
+left = pyray.load_texture_from_image(pyray.load_image("./tiles/left.png"))
+
 
 while not pyray.window_should_close():
     pyray.begin_drawing()
-    pyray.clear_background(pyray.WHITE)
-    pyray.draw_text("Hello world", 190, 200, 20, pyray.VIOLET)
     pyray.end_drawing()
 
 pyray.close_window()
