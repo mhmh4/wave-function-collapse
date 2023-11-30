3c5d70cfa0144f356e6c91e2b1d8d27c491f108c
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-30 11:36:49 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -20,10 +20,30 @@ RULES = [
         [BLANK, DOWN],
         [BLANK, LEFT],
     ],
-    [[RIGHT, LEFT, DOWN], [LEFT, UP, DOWN], [BLANK, DOWN], [RIGHT, UP, DOWN]],
-    [[RIGHT, LEFT, DOWN], [LEFT, UP, DOWN], [RIGHT, LEFT, UP], [BLANK, LEFT]],
-    [[BLANK, UP], [LEFT, UP, DOWN], [RIGHT, LEFT, UP], [RIGHT, UP, DOWN]],
-    [[RIGHT, LEFT, DOWN], [BLANK, RIGHT], [RIGHT, LEFT, UP], [UP, DOWN, RIGHT]],
+    [
+        [RIGHT, LEFT, DOWN],
+        [LEFT, UP, DOWN],
+        [BLANK, DOWN],
+        [RIGHT, UP, DOWN],
+    ],
+    [
+        [RIGHT, LEFT, DOWN],
+        [LEFT, UP, DOWN],
+        [RIGHT, LEFT, UP],
+        [BLANK, LEFT],
+    ],
+    [
+        [BLANK, UP],
+        [LEFT, UP, DOWN],
+        [RIGHT, LEFT, UP],
+        [RIGHT, UP, DOWN],
+    ],
+    [
+        [RIGHT, LEFT, DOWN],
+        [BLANK, RIGHT],
+        [RIGHT, LEFT, UP],
+        [UP, DOWN, RIGHT],
+    ],
 ]
 
 pyray.init_window(WIDTH, HEIGHT, "")
